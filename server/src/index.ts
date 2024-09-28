28e915a0174746f8e12ada57bb720e16d6b48535
Pranjal360Agarwal <pranjal360agarwal@gmail.com>
2024-09-28 16:41:20 +0530

    Create user route post trigger

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,16 @@
+
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-
+/* ROUTE IMPORTS */
+import projectRoutes from "./routes/projectRoutes";
+import taskRoutes from "./routes/taskRoutes";
+import searchRoutes from "./routes/searchRoutes";
+import userRoutes from "./routes/userRoutes";
+import teamRoutes from "./routes/teamRoutes";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -22,6 +28,11 @@ app.get("/", (req, res) => {
   res.send("This is home route");
 });
 
+app.use("/projects", projectRoutes);
+app.use("/tasks", taskRoutes);
+app.use("/search", searchRoutes);
+app.use("/users", userRoutes);
+app.use("/teams", teamRoutes);
 
 /* SERVER */
 const port = Number(process.env.PORT) || 3000;
